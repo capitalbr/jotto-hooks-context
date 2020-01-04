4eb345a17c202c0d3d3ac8d953a39229adab7b62
Bradley <bbarnes.me@gmail.com>
2020-01-04 16:35:03 -0600

    Add code and tests for conditional rendering of App/Spinner with presence of secretWord

--- src/components/App.js
+++ src/components/App.js
@@ -13,25 +13,45 @@ const reducer = (state, action) => {
 };
 
 const initialState = {
-  secretWord: ""
+  // empty string shows no error on PropTypes but null would have which would have
+  // alerted me to the fact that I probably need a spinner here in case of network
+  // latency
+  secretWord: null
 }
 
 function App() {
+  // like a single reducer version of a redux store
   const [state, dispatch] = React.useReducer(reducer, initialState);
 
+  // basically like then returned promose part of a redux action after the
+  // asynchronous api call has returned it's response
   const setSecretWord = (secretWord) => {
     dispatch({ type: "setSecretWord", payload: secretWord});
   };
 
   React.useEffect(() => {
+    //  This is like the first part of a redux action, passing in the second part
+    // in this case the second part takes in the argument that changes the state
     hookActions.getSecretWord(setSecretWord);
   }, []);
 
   return (
-    <div data-test="component-app">
-      <Input />
-    </div>
-  );
+    state.secretWord 
+    ? (
+      <div className="container" data-test="component-app">
+        <Input secretWord={state.secretWord} />
+      </div>
+    ) 
+    : ( 
+      <div 
+        className="container" data-test="spinner">
+        <div className="spinner-border" role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+        <p>Loading secret word</p>
+      </div>
+    )
+  )
 }
 
 export default App;
--- src/components/App.test.js
+++ src/components/App.test.js
@@ -8,14 +8,29 @@ import hookActions from "../actions/hookActions";
 
 const mockGetSecretWord = jest.fn();
 
-const setup = () => {
+const setup = (secretWord) => {
   mockGetSecretWord.mockClear();
   hookActions.getSecretWord = mockGetSecretWord;  
+
+  // all this is doing is making the 'state' variable have the desired store shape,
+  // meaning it has a key of 'secretWord' with the value of our choice
+  const mockUseReducer = jest.fn(() => {
+    return (
+      [
+        { secretWord },
+        jest.fn()
+      ]
+    )
+  });
+
+  React.useReducer = mockUseReducer
   return mount(<App />);
+
 };
 
 test('renders without error', () => {
-  const wrapper = setup();
+  // needs secretWord in state to render app component so pass a string to setup
+  const wrapper = setup("party");
   const appComponent = findByTestAttr(wrapper, "component-app")
   expect(appComponent.length).toBe(1);
 });
@@ -42,4 +57,49 @@ describe("getSecretWord", () => {
     expect(mockGetSecretWord).not.toHaveBeenCalled();
   });
 });
+
+
+
+
+describe("when secet word is a non empty string", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = setup("party");
+  })
+  
+  test("renders app component", () => {
+    const appComponent = findByTestAttr(wrapper, "component-app");
+    expect(appComponent.exists()).toBe(true);
+  });
+  
+  test("does not render spinner", () => {
+    const spinner = findByTestAttr(wrapper, "spinner");
+    expect(spinner.exists()).toBe(false);
+  })
+
+});
+
+describe("when secret word is undefined, an empty string, or null", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = setup(null);
+  })
+
+  test("does not render app component", () => {
+    const appComponent = wrapper.find("[data-test='component-app']");
+    expect(appComponent.exists()).toBe(false);
+  });
+
+  test("does render spinner", () => {
+    const spinner = wrapper.find("[data-test='spinner']");
+    expect(spinner.exists()).toBe(true);
+  });
+
+});
+
+
+
+
 
